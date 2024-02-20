d9833e0abf7eaaeb01951786442feb93b5d7f16e
AdrikLarreategui <adrian15186@hotmail.com>
2024-02-20 13:42:43 +0100

    commit a meter cambios en TheHeader.jsx y Products.jsx para añadir el carrito. Añadido estilos para products. Creación de Login.jsx

--- EcommerceReactFront/src/components/Header/TheHeader.jsx
+++ EcommerceReactFront/src/components/Header/TheHeader.jsx
@@ -1,14 +1,56 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { UsersContext } from '../../context/UsersContext/UsersState'
+import { Link, useNavigate } from 'react-router-dom'
+import {
+    LoginOutlined,
+    LogoutOutlined,
+    ProductOutlined,
+    UserOutlined,
+    ShoppingCartOutlined
+} from '@ant-design/icons'
 import './TheHeader.styles.scss'
 
 const TheHeader = () => {
+    const navigate = useNavigate()
+    const { token, logout } = useContext(UsersContext)
+
+    const logoutUser = () => {
+        logout()
+        setTimeout(() => {
+            navigate('/')
+        }, 2000)
+    }
+
     return (
-        <nav className='header'>
-                <Link className='header__link' to ='/'>Home</Link>
-                <Link className='header__link' to = '/form'>Register</Link>
-                <Link className='header__link' to = ''>Login</Link>
-                <Link className='header__link' to = ''>Profile</Link>
+        <nav className='TheHeader'>
+            <div>
+                {token ? (
+                    <>
+                        <span className='menu-item' onClick={ logoutUser }>
+                            <LogoutOutlined />
+                            <Link to='/'>Logout</Link>
+                        </span>
+                        <span className='menu-item'>
+                            <ProductOutlined />
+                            <Link to= '/products'>Products</Link>
+                        </span>
+                        <span className='menu-item'>
+                            <ShoppingCartOutlined />
+                            <Link to='/cart'>Cart</Link>
+                        </span>
+                        <span className='menu-item'>
+                            <UserOutlined />
+                            <Link to='/profile'>Profile</Link>
+                        </span>
+                    </>
+                ) : (
+                    <span className='menu-item'>
+                        <LoginOutlined />
+                        <Link to='/'>Login</Link>
+                    </span>
+                )}
+            </div>
         </nav>
     )
 }
@@ -16,3 +58,15 @@ const TheHeader = () => {
 export default TheHeader
 
 // En la línea 10, la de register, ¿el enlace es a Form?
+
+   // return (
+    //     <nav className='header'>
+    //         <div>
+    //             {token ? ()}
+    //             <Link className='header__link' to ='/'>Home</Link>
+    //             <Link className='header__link' to = '/form'>Register</Link>
+    //             <Link className='header__link' to = ''>Login</Link>
+    //             <Link className='header__link' to = ''>Profile</Link>
+    //         </div>
+    //     </nav>
+    // )
--- EcommerceReactFront/src/components/Products/Products.jsx
+++ EcommerceReactFront/src/components/Products/Products.jsx
@@ -1,22 +1,41 @@
 import React, { useContext, useEffect } from "react";
 import { ProductsContext } from "../../context/ProductsContext/ProductsState";
+import { Card, Col } from 'antd'
+import '/Products.styles.scss'
 
 const Products = () => {
-    const { getProducts, products } = useContext(ProductsContext);
+    const { getProducts, products, addCart } = useContext(ProductsContext);
     useEffect(() => {
         getProducts();
     }, []);
 
-const product = products.map((product) => {
-    return (
-        <div key={product._id}>
-        <span>{product.name} </span>
-        <span>{product.price.toFixed(2)}€</span>
-        <button onClick={() => addCart(product)}>Add Cart</button>
-        </div>
-);
-});
-return <div>{product}</div>;
+    return(
+        <>
+        <h1>Products</h1>
+            <section className="products">
+                {products.map((product) => (
+                    <Col span={8} key={product._id}>
+                        <Card title={product.name} bordered={false}>
+                            <p>price: {product.price.tofixed(2)}€</p>
+                            <button onClick={() => addCart(product)}>addCart</button>
+                        </Card>
+                    </Col>
+                ))}
+            </section>
+        </>
+    )
+
 };
 
 export default Products;
+
+// const product = products.map((product) => {
+// //     return (
+// //         <div key={product._id}>
+// //         <span>{product.name} </span>
+// //         <span>{product.price.toFixed(2)}€</span>
+// //         <button onClick={() => addCart(product)}>Add Cart</button>
+// //         </div>
+// // );
+// });
+// return <div>{product}</div>;
