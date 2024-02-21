5377367047543bde20a7ca32284b599364aec6e2
danirus6 <danirus6@gmail.com>
2024-02-21 19:56:31 +0100

    formTemporal

--- EcommerceReactFront/src/components/Form/TheForm.jsx
+++ EcommerceReactFront/src/components/Form/TheForm.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import './TheForm.styles.scss'
-import Swal from 'sweetalert2'
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './TheForm.styles.scss';
+import Swal from 'sweetalert2';
 
 const TheForm = () => {
     const [data, setData] = useState({
@@ -10,62 +10,48 @@ const TheForm = () => {
         username: '',
         email: '',
         password: ''
-    })
+    });
 
-    let navigate = useNavigate()
-    let localData = JSON.parse(localStorage.getItem('data')) //Esto es para guardarlo en el navegador
+    let navigate = useNavigate();
 
     const handleInputChange = (event) => {
-        setData({...data, [ event.target.name ]: event.target.name})
+        setData({ ...data, [event.target.name]: event.target.value });
     }
 
     const handleSubmit = (event) => {
-        event.preventDefault()
-        if(data.name && data.username && data.password) {
-            if(localData == !null) {
-                localData.push({
-                    name: data.name,
-                    username: data.username,
-                    email: data.email,
-                    password: data.password
-                })
-            } else {
-                localData = [{
-                    name: data.name,
-                    username: data.username,
-                    email: data.email,
-                    password: data.password
-                }]
-            }
+        event.preventDefault();
 
-        localStorage.setItem('data', JSON.stringify(localData))
-        console.log({ message: 'data succesfully stored: '}, { name: data.name,
-            username: data.username, email: data.email, password: data.password })
-        setTimeout(()=> {navigate('/list')}, 2000) // El enlace a list habrá que cambiar.
-        }
-    }
-
-    const Alert = () => {
-        if(data.name.length < 3 || data.username.length < 4 || data.password.length < 8 || data.email.length <6) { //El límite de caracteres puede variar
-            Swal.fire({  //Introducir cambios para que sea necesario usar caracteres especiales en el password
+        // Validaciones
+        if (data.name.length < 3 || data.username.length < 4 || data.password.length < 8 || data.email.length < 6) {
+            Swal.fire({
                 title: 'Error!',
-                text: 'Rellena todos los campos!',
-                confirmButtonText: 'Aceptar'  
-            })
+                text: 'Rellena todos los campos correctamente!',
+                confirmButtonText: 'Aceptar'
+            });
+            return; // Detener el envío si las validaciones fallan
         }
+
+        // Lógica con localStorage (simplificada)
+        let localData = JSON.parse(localStorage.getItem('data')) || [];
+        localData.push(data); 
+        localStorage.setItem('data', JSON.stringify(localData));
+
+        // Éxito
+        console.log('Datos guardados:', data); 
+        navigate('/list', { replace: true }); // Usar replace para una correcta redirección
     }
 
     return (
-            <form className='form' onSubmit={handleSubmit}>
-                <div className='form__data'>
-                    <input className='form__data__box' placeholder='name' type='text' name='name' value={data.name} onChange={handleInputChange}/>
-                    <input className='form__data__box' placeholder='username' type= 'text' name='username' value={data.username} onChange={handleInputChange}/>
-                    <input className='form__data__box' placeholder='email' type= 'email' name='email' value={data.email} onChange={handleInputChange}/>
-                    <input className='form__data__box' placeholder='password' type='text' name='password' value={data.password} onChange={handleInputChange}/>
-                </div>
-            <button type='submit' onClick={Alert}>Submit</button>
-            </form>
-    )
+        <form className='form' onSubmit={handleSubmit}>
+            <div className='form__data'>
+                <input className='form__data__box' placeholder='name' type='text' name='name' value={data.name} onChange={handleInputChange}/>
+                <input className='form__data__box' placeholder='username' type= 'text' name='username' value={data.username} onChange={handleInputChange}/>
+                <input className='form__data__box' placeholder='email' type= 'email' name='email' value={data.email} onChange={handleInputChange}/>
+                <input className='form__data__box' placeholder='password' type='text' name='password' value={data.password} onChange={handleInputChange}/>
+            </div>
+            <button type='submit'>Submit</button>
+        </form>
+    );
 }
 
-export default TheForm
+export default TheForm;
